75d00a39fe5a5f7a4770eecdf3fa1118b7e23ecf
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-06 11:58:49 +0300

    Default Changelist index.js changed;

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-
-setTimeout((message) => {
+const n1 = setTimeout((message) => {
   console.log(message);
 }, 1400, 'yahoo!');
+console.log(': ', n1);
 
-setTimeout(() => {
+const n2 = setTimeout(() => {
   console.log(': hey!');
-}, 100);
+}, 1000);
+console.log(': ', n2);
+
+setInterval(() => {
+
+}, 1500,
+  'hi');
+
