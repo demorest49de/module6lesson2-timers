a73632314afa6f7c4cca18aa3350a2d8eae05766
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-06 18:44:30 +0300

    Default Changelist index.js changed;

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const n1 = setTimeout((message) => {
-  console.log(message);
-}, 1400, 'yahoo!');
-console.log(': ', n1);
+// const n1 = setTimeout((message) => {
+//   console.log(message);
+// }, 1400, 'yahoo!');
+// console.log(': ', n1);
+//
+// const n2 = setTimeout(() => {
+//   console.log(': hey!');
+// }, 1000);
+// console.log(': ', n2);
+//
+// const n3 = setInterval((str) => {
+//     console.log(': ',str);
+//   }, 1500,
+//   'hi');
 
-const n2 = setTimeout(() => {
-  console.log(': hey!');
-}, 1000);
-console.log(': ', n2);
-
-setInterval(() => {
-
-}, 1500,
-  'hi');
-
+let count = 0;
+let timerId = setTimeout(function tick() {
+  count++;
+  console.log(`yahoo: `, count);
+  if (count < 3) {
+    setTimeout(tick, 1000);
+  } else {
+    console.log('timer stop at: ', count);
+    clearInterval(timerId);
+  }
+});
