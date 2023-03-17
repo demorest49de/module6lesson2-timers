75fa0ff4fde726831fb63e63bbe52d26ba303264
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-17 17:01:13 +0300

    Changes control.js changed; timer.css changed;

--- modules/control.js
+++ modules/control.js
@@ -8,7 +8,7 @@ export  const delayOutput = () => {
   const timerOutput = document.querySelector('.timer__input');
   const p = document.querySelector('.timer__paragraph');
 
-  timerOutput.addEventListener('input', (e) => {
+  timerOutput.addEventListener('keyup', (e) => {
     setDelayText(() => {
       const target = e.target;
       p.textContent = target.value;
