b991e6c80f9baf24f38970a9eee71e2f2945a165
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-21 14:06:28 +0300

    Default Changelist accordion.css renamed, changed; control.js changed; create.js changed; index.css changed; index.html changed; Inter-Bold.ttf removed; Inter-Regular.ttf removed; normalize.css removed;

--- modules/control.js
+++ modules/control.js
@@ -1,13 +1,3 @@
 export const delayOutput = () => {
-    const timerInput = document.querySelector('.timer__input');
-    const p = document.querySelector('.timer__paragraph');
-    timerInput.focus();
-    let timeoutId = 0;
-    timerInput.addEventListener('keyup', () => {
-        clearTimeout(timeoutId);
 
-        timeoutId = setTimeout(() => {
-            p.textContent = timerInput.value;
-        }, 300);
-    });
 };
--- modules/create.js
+++ modules/create.js
@@ -3,17 +3,7 @@ export const createSection = () => {
   section.classList.add('timer');
   section.insertAdjacentHTML("beforeend", `
     <div class="container">
-      <div class="timer__title-block">
-        <h1 class="timer__title">Project for testing and edicational purposes</h1>
-        <label class="timer__label"> <span class="timer__text-label">Put some text:</span>
-          <input class="form-control timer__input" type="text" name="timer__input">
-        </label>
-      </div>
-      <div class="timer__subblock">
-        <label class="timer__label-block">Output text here:</label>
-        <p class="timer__paragraph">
-        </p>
-      </div>
+      
     </div>
   `);
 
