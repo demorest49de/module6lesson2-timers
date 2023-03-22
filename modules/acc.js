94c351ba367aa9bd4943282b837a64c11725ce40
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-22 23:00:44 +0300

    Changes acc.js changed; accordion.css changed; index.html changed; open.svg added; open-active.svg added; open-hover.svg added; open-purple.svg added; open-purple-active.svg added; open-purple-hover.svg added;

--- modules/acc.js
+++ modules/acc.js
@@ -1,11 +1,4 @@
 export const handleAcc = () => {
-    const list = document.querySelectorAll('.li-question');
-    console.log(' : ', list);
 
-    list.forEach((listElem, index) => {
-        listElem.addEventListener('click', ({target}) => {
-
-        });
-    });
 };
 
