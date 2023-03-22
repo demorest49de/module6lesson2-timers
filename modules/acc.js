7237260f6febd8e0c4b0211f567fe52730ed75dc
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-23 00:47:03 +0300

    Changes acc.js changed; accordion.css changed; index.html changed;

--- modules/acc.js
+++ modules/acc.js
@@ -1,11 +1,29 @@
 export const handleAcc = () => {
     const liElems = document.querySelectorAll(".li-question");
-    const btns = document.querySelectorAll("button");
+    const btns = document.querySelectorAll(".button");
+    const txtWrps = document.querySelectorAll(".text-wrapper");
 
-    liElems.forEach((li, indexLi) => {
-        btns.forEach((btn, indexBtn) => {
+    let wrpHeight = 0;
 
+    txtWrps.forEach(elem => {
+        if (elem.scrollHeight > wrpHeight) {
+            wrpHeight = elem.scrollHeight;
+        }
+    });
+    console.log(' : ', wrpHeight);
+    liElems.forEach((li, iLi) => {
+        btns.forEach((btn, iBtn) => {
+            btn.addEventListener("click", () => {
+                if (iLi === iBtn) {
+                    txtWrps[iBtn].style.height =
+                        li.classList.contains('li-question-active') ?
+                            '' : `${wrpHeight}px`;
+                    li.classList.toggle('li-question-active');
+                } else {
+                    li.classList.remove('li-question-active');
+                    txtWrps[iBtn].style.height = '';
+                }
+            });
         });
     });
 };
-
