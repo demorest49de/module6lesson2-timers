7569562c618f24483ed73cc474e15334b1ed3f62
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-22 15:39:47 +0300

    Default Changelist acc.js changed; accordion.css changed; create.js changed;

--- modules/acc.js
+++ modules/acc.js
@@ -1,6 +1,9 @@
+export const handleAcc = () => {
+    const list = document.querySelector('ul.list');
+    list.addEventListener('click', ({target}) => {
+        if (target.classList.contains('question')) {
 
-export const handleAcc = () =>{
-    const question = document.getElementsByClassName('question');
-    console.log(' : ',question);
-    question
-}
+        }
+    });
+};
+
--- modules/create.js
+++ modules/create.js
@@ -6,9 +6,15 @@ export const createSection = () => {
           <ul class="list">
               <li class="question">
                   <div class="block">
-                      <button class="button" type="button">item 1</button>
+                      <button class="button" type="button">item 1?</button>
                       <div class="wrapper">
-                          <span class="text">some text</span>
+                          <span class="text">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
+                          </span>
                       </div>
                   </div>
               </li>
