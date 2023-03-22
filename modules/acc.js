ce0ae75458542dd78bd5cf0317345a0d89de025d
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-22 23:28:03 +0300

    Changes acc.js changed; accordion.css changed; create.js changed; render.js deleted;

--- modules/acc.js
+++ modules/acc.js
@@ -1,4 +1,11 @@
 export const handleAcc = () => {
+    const liElems = document.querySelectorAll(".li-question");
+    const btns = document.querySelectorAll("button");
 
+    liElems.forEach((li, indexLi) => {
+        btns.forEach((btn, indexBtn) => {
+
+        });
+    });
 };
 
--- modules/create.js
+++ modules/create.js
@@ -2,68 +2,7 @@ export const createSection = () => {
     const section = document.createElement('section');
     section.classList.add('accordion');
     section.insertAdjacentHTML("beforeend", `
-      <div class="container">
-          <ul class="list">
-              <li class="li-question">
-                  <div class="block">
-                      <button class="button" type="button">item 1?</button>
-                      <div class="wrapper">
-                          <span class="text">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          </span>
-                      </div>
-                  </div>
-              </li>
-              <li class="li-question">
-                  <div class="block">
-                      <button class="button" type="button">item 2</button>
-                      <div class="wrapper">
-                          <span class="text">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          </span>
-                      </div>
-                  </div>
-              </li>
-              <li class="li-question">
-                  <div class="block">
-                      <button class="button" type="button">item 3</button>
-                      <div class="wrapper">
-                          <span class="text">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          </span>
-                      </div>
-                  </div>
-              </li>
-              <li class="li-question">
-                  <div class="block">
-                      <button class="button" type="button">item 3</button>
-                      <div class="wrapper">
-                          <span class="text">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, sunt?
-                          </span>
-                      </div>
-                  </div>
-              </li>
-          </ul>
-      </div>
-  `);
+        
+    `);
     document.body.appendChild(section);
 };
