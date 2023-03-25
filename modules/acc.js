b340697b2a20962a9ecb618492fcf0cd9806a1dd
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-25 15:52:14 +0300

    Default Changelist accordion.css changed; index.html changed;

--- modules/acc.js
+++ modules/acc.js
@@ -1,29 +1,14 @@
 export const handleAcc = () => {
-    const liElems = document.querySelectorAll(".li-question");
-    const btns = document.querySelectorAll(".button");
-    const txtWrps = document.querySelectorAll(".text-wrapper");
+    const list = document.querySelector('.list');
+    const activeQuestion = document.getElementsByClassName('li-question-active');
 
-
-    let wrpHeight = 0;
-
-    txtWrps.forEach(elem => {
-        if (elem.scrollHeight > wrpHeight) {
-            wrpHeight = elem.scrollHeight;
+    list.addEventListener("click", ({target}) => {
+        if (activeQuestion[0] && activeQuestion[0] !== target.closest('.li-question')) {
+            activeQuestion[0].classList.remove('li-question-active');
         }
-    });
 
-    liElems.forEach((li, iLi) => {
-        btns.forEach((btn, i) => {
-            btn.addEventListener("click", () => {
-                if (iLi === i) {
-                    li.classList.toggle('li-question-active');
-                    console.log(' : ', li);
-                    txtWrps[i].style.height = li.classList.contains('li-question-active') ?
-                        `${wrpHeight}px` : `0px`;
-                } else {
-                    li.classList.remove('li-question-active');
-                }
-            });
-        });
+        if (target.closest('.button')) {
+            target.closest('.li-question').classList.toggle('li-question-active');
+        }
     });
 };
